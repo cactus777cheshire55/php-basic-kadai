f65bb71e29e0977a4cef38d1cfe78e2ca4a9756e
cactus777cheshire55 <126995348+cactus777cheshire55@users.noreply.github.com>
2026-05-22 04:40:34 +0900

    Update foreach.php

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -9,7 +9,7 @@
     <p>
       <?php 
       $merchandise = [
-        'id' => '1', '名前' => '侍太郎', '年齢' => 30];
+        '名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
 
         foreach ($merchandise as $index => $value) {
           echo "{$index} : {$value} <br>";
