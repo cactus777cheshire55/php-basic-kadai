7106f9a28e1e92f6b47d8fe04706a8fb79e5530c
cactus777cheshire55 <126995348+cactus777cheshire55@users.noreply.github.com>
2026-05-22 01:11:42 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title></title>
+
+</head>
+
+<body>
+  <p>
+
+  <?php 
+    $kadai = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    print_r($kadai);
+  ?>
+
+  </p>
+</body>
+
+</html>
